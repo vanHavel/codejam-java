8bf669b9bce3dea078d475abd083cf0f0a1021fe
Lukas <lukashuwald@online.de>
2018-02-10 00:43:50 +0100

    3d point

---
+++ utility/Point3D.java
@@ -0,0 +1,21 @@
+package utility;
+
+public class Point3D {
+    public double x;
+    public double y;
+    public double z;
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    static double manhattanDistance(Point3D p1, Point3D p2) {
+        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y) + Math.abs(p1.z - p2.z);
+    }
+
+    static double distance(Point3D p1, Point3D p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
+    }
+}
