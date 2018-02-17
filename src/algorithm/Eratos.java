d2d30ce49ad72cda3d8162344efbaae14624cb96
Lukas <lukashuwald@online.de>
2018-02-17 16:12:47 +0100

    increased efficiency of eratos

--- src/algorithm/Eratos.java
+++ src/algorithm/Eratos.java
@@ -7,43 +7,42 @@ import java.util.Vector;
  */
 
 // this class implements the prime number sieve of Eratosthenes
+// use this for bounds n up to 10^8
 public class Eratos {
 
     private boolean[] isPrime;
     private Vector<Integer> primes;
+    private int bound;
 
     // initialize eratos with upper bound
     public Eratos(int bound) {
-        this.primes = new Vector<>();
-        this.isPrime = new boolean[bound+1];
+        this.primes = new Vector<>(1000000);
+        this.bound = bound;
+        int odds = (bound / 2) + (bound % 2);
+        // is Prime stores primality for all odd numbers up to bound
+        this.isPrime = new boolean[odds];
     }
 
     // perform sieve to find all primes up to (and including) bound
     public void sieve() {
-        int bound = this.isPrime.length;
-        if ((bound - 1) < 2) {
+        if ((this.bound - 1) < 2) {
             return;
         }
-        for (int i = 3; i < bound; ++i) {
+        for (int i = 1; i < this.isPrime.length; ++i) {
             this.isPrime[i] = true;
         }
         this.primes.add(2);
 
         // only consider odd numbers > 2
-        for (int i = 3; i < bound; i += 2) {
-            if (!this.isPrime[i]) {
+        for (int i = 3; i <= bound; i += 2) {
+            if (!this.isPrime[i / 2]) {
                 continue;
             }
             primes.add(i);
-            for (int j = 3*i; j < bound; j += 2*i) {
-                isPrime[j] = false;
+            for (int j = 3*i; j <= bound; j += 2*i) {
+                isPrime[j / 2] = false;
             }
         }
-    }
-
-    // returns boolean array of primality statuses
-    public boolean[] getIsPrime() {
-        return this.isPrime;
     }
 
     // returns vector of all primes sieved
