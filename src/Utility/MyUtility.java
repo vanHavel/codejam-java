65b78c886c61fea7bb7bb4d51cd8473292136a81
Lukas <lukashuwald@online.de>
2018-02-22 10:33:37 +0100

    addded method to compute all combinations up to n, k

--- src/Utility/MyUtility.java
+++ src/Utility/MyUtility.java
@@ -20,7 +20,8 @@ public class MyUtility {
         };
     }
 
-    // calculate binomial coefficients modulo modulus
+    // calculate binomial coefficient (n choose k) modulo modulus
+    // implemented separately from combinations because we can do this in O(k) space
     public static long nChooseK(int n, int k, long modulus) {
         long[] dp = new long[k+1];
         dp[0] = 1;
@@ -30,5 +31,18 @@ public class MyUtility {
             }
         }
         return dp[k];
+    }
+
+    // calculate all binomial coefficients until n, k modulo modulus
+    public static long[][] combinations(int n, int k, long modulus) {
+        long[][] dp = new long[n+1][k+1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = k; j >= 1; --j) {
+                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % modulus;
+            }
+            dp[i][0] = 1;
+        }
+        return dp;
     }
 }
--- test/Utility/MyUtilityTest.java
+++ test/Utility/MyUtilityTest.java
@@ -31,4 +31,17 @@ class MyUtilityTest {
         assertEquals(0, MyUtility.nChooseK(5, 3, 5));
         assertEquals(6, MyUtility.nChooseK(10, 2, 13));
     }
+
+    @Test
+    void testCombinations() {
+        long[][] combinations = MyUtility.combinations(10, 10, 13);
+        // ignoring modulus
+        assertEquals(1, combinations[0][0]);
+        assertEquals(2, combinations[2][1]);
+        assertEquals(10, combinations[5][3]);
+        // using modulus
+        assertEquals(6, combinations[10][2]);
+        assertEquals(7, combinations[6][3]);
+        assertEquals(1, combinations[10][10]);
+    }
 }
