d2654685a324dad3de761906d0f3d11676def58f
Lukas <lukashuwald@online.de>
2018-02-22 10:18:23 +0100

    added test for stringAsList

--- test/Utility/MyUtilityTest.java
+++ test/Utility/MyUtilityTest.java
@@ -2,13 +2,23 @@ package Utility;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyUtilityTest {
 
     @Test
     void testStringAsList() {
-
+        List<Character> s = new LinkedList<>();
+        assertEquals(s, MyUtility.stringAsList(""));
+        s.add('c');
+        assertEquals(s, MyUtility.stringAsList("c"));
+        s.add('c');
+        assertEquals(s, MyUtility.stringAsList("cc"));
+        s.add('a');
+        assertEquals(s, MyUtility.stringAsList("cca"));
     }
 
     @Test
