64bb9cab363141f674fe4296c4bffed5c7bbfcd8
Lukas <lukashuwald@online.de>
2018-03-29 17:46:11 +0200

    added trinary search

--- src/utility/MyUtility.java
+++ src/utility/MyUtility.java
@@ -2,6 +2,7 @@ package utility;
 
 import java.util.AbstractList;
 import java.util.List;
+import java.util.function.LongFunction;
 
 public class MyUtility {
 
@@ -20,6 +21,7 @@ public class MyUtility {
         };
     }
 
+    // returns minimum in long array
     public static long arrayMin(long[] a) {
         long best = Long.MAX_VALUE;
         for (int i = 0; i < a.length; ++i) {
@@ -28,12 +30,26 @@ public class MyUtility {
         return best;
     }
 
+    // returns maximum in long array
     public static long arrayMax(long[] a) {
         long best = Long.MIN_VALUE;
         for (int i = 0; i < a.length; ++i) {
             best = Math.max(a[i], best);
         }
         return best;
+    }
+
+    // performs trinary search to obtain maximum of unimodal function
+    public static <A extends Comparable<A>> long trinarySearch(LongFunction<A> f, long lo, long hi) {
+        while (lo < hi) {
+            long mid = lo + (hi - lo) / 2;
+            if (f.apply(mid).compareTo(f.apply(mid + 1)) < 0) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
     }
 
 }
