2246fdf67d5578ac0c18768571ba56d7be3a721d
Lukas <lukashuwald@online.de>
2018-04-08 17:44:13 +0200

    added Kruskal's algorithm

--- src/algorithm/MinimumSpanningTree.java
+++ src/algorithm/MinimumSpanningTree.java
@@ -1,13 +1,15 @@
 package algorithm;
 
 import data.Edge;
+import structure.UnionFind;
 
-import java.util.Set;
+import java.util.*;
 
+// thi class handles computing minimum spanning trees in undirected graphs
 public class MinimumSpanningTree {
 
     private int[][] weights;
-    private Set<Edge> edges;
+    private Set<Edge> mstEdges;
     private int cost;
 
     // initialize with weights array, use -1 for non-existing edges
@@ -17,12 +19,34 @@ public class MinimumSpanningTree {
 
     // compute a MST using Kruskal's algorithm
     public void computeMST() {
-        //TODO
+        int n = this.weights.length;
+        Queue<Edge> edges = new PriorityQueue<>(n*n,
+                Comparator.comparing(e -> this.weights[e.origin][e.target]));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                if (this.weights[i][j] != -1) {
+                    edges.add(new Edge(i,j));
+                }
+            }
+        }
+
+        this.mstEdges = new HashSet<>();
+        UnionFind uf = new UnionFind(n);
+        
+        while (this.mstEdges.size() < n - 1) {
+            Edge top = edges.poll();
+            if (uf.find(top.origin) != uf.find(top.target)) {
+                uf.union(top.origin, top.target);
+                this.cost += this.weights[top.origin][top.target];
+                this.mstEdges.add(top);
+            }
+        }
     }
 
     // get edges of mst
     public Set<Edge> getEdges() {
-       return this.edges;
+       return this.mstEdges;
     }
 
     // get cost of mst
