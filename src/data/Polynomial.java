61ddcf4f27ebad629f2f9dd6c170317dc8afdd76
Lukas <lukashuwald@online.de>
2018-04-23 11:57:10 +0200

    more polynomial unit tests, fixed some bugs

--- src/data/Polynomial.java
+++ src/data/Polynomial.java
@@ -5,6 +5,9 @@ import algorithm.FastFourierTransformer;
 // this class handles polynomials with complex coefficients
 public class Polynomial {
 
+    // numbers with absolute value smaller then epsilon are treated like 0
+    private static final double EPSILON = 1e-12;
+
     public final Complex[] coefficients;
 
     public Polynomial(Complex[] coefficients) {
@@ -19,6 +22,7 @@ public class Polynomial {
         int n = Math.max(p1.coefficients.length, p2.coefficients.length);
         Complex[] addedCoefficients = new Complex[n];
         for (int i = 0; i < n; ++i) {
+            addedCoefficients[i] = Complex.ZERO;
             if (i < p1.coefficients.length) {
                 addedCoefficients[i] = addedCoefficients[i].add(p1.coefficients[i]);
             }
@@ -41,15 +45,30 @@ public class Polynomial {
             coefficients1[i] = i < p1.coefficients.length ? p1.coefficients[i] : Complex.ZERO;
             coefficients2[i] = i < p2.coefficients.length ? p2.coefficients[i] : Complex.ZERO;
         }
+        // fft
         Complex[] fft1 = FastFourierTransformer.fft(coefficients1);
         Complex[] fft2 = FastFourierTransformer.fft(coefficients2);
+        // multiply
         int n = fft1.length;
         Complex[] multipliedValuations = new Complex[n];
         for (int i = 0; i < n; ++i) {
             multipliedValuations[i] =  fft1[i].multiply(fft2[i]);
         }
+        // inverse fft
         Complex[] productCoefficients = FastFourierTransformer.inverseFft(multipliedValuations);
-        return new Polynomial(productCoefficients);
+        // remove trailing zero coefficients
+        int last = productCoefficients.length;
+        while (last > 0 && productCoefficients[last-1].abs() < EPSILON) {
+            last--;
+        }
+        if (last == productCoefficients.length) {
+            return new Polynomial(productCoefficients);
+        }
+        Complex[] finalCoefficients = new Complex[last];
+        for (int i = 0; i < last; ++i) {
+            finalCoefficients[i] = productCoefficients[i];
+        }
+        return new Polynomial(finalCoefficients);
     }
 
     // evaluate with horner schema
@@ -59,7 +78,7 @@ public class Polynomial {
     public static Complex evaluate(Polynomial p, Complex x) {
         int n = p.coefficients.length;
         Complex res = p.coefficients[n-1];
-        for (int i = n-2; n >= 0; --i) {
+        for (int i = n-2; i >= 0; --i) {
             res = res.multiply(x);
             res = res.add(p.coefficients[i]);
         }
--- test/data/PolynomialTest.java
+++ test/data/PolynomialTest.java
@@ -17,6 +17,7 @@ class PolynomialTest {
         assertEquals(3, product.coefficients[0].real, 0.01);
         assertEquals(7, product.coefficients[1].real, 0.01);
         assertEquals(2, product.coefficients[2].real, 0.01);
+        assertEquals(3, product.coefficients.length);
 
         Polynomial biggerProduct = product.multiply(product);
         assertEquals(9, biggerProduct.coefficients[0].real, 0.01);
@@ -24,6 +25,34 @@ class PolynomialTest {
         assertEquals(61, biggerProduct.coefficients[2].real, 0.01);
         assertEquals(28, biggerProduct.coefficients[3].real, 0.01);
         assertEquals(4, biggerProduct.coefficients[4].real, 0.01);
+        assertEquals(5, biggerProduct.coefficients.length);
+    }
+
+    @Test
+    public void testAddition() {
+        // x^2 + 2x + 3
+        Polynomial p1 = new Polynomial(new Complex[]{
+                Complex.realNumber(3), Complex.realNumber(2), Complex.realNumber(1)
+        });
+        // x + i
+        Polynomial p2 = new Polynomial(new Complex[]{Complex.I, Complex.ONE});
+
+        Polynomial sum = p1.add(p2);
+        assertEquals(new Complex(3, 1), sum.coefficients[0]);
+        assertEquals(new Complex(3, 0), sum.coefficients[1]);
+        assertEquals(new Complex(1, 0), sum.coefficients[2]);
+        assertEquals(3, sum.coefficients.length);
+    }
+
+    @Test
+    public void testEvaluation() {
+        // x^2 + 2x + 3
+        Polynomial p1 = new Polynomial(new Complex[] {
+                Complex.realNumber(3), Complex.realNumber(2), Complex.realNumber(1)
+        });
+        assertEquals(Complex.realNumber(3), p1.evaluate(Complex.realNumber(0)));
+        assertEquals(Complex.realNumber(38), p1.evaluate(Complex.realNumber(5)));
+        assertEquals(new Complex(2, 2), p1.evaluate(Complex.I));
     }
 
 }
