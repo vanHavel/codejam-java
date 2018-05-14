c7e44a0681be85d3c2bb75f0a90a881385fd6ca7
Lukas <dev.lukas.huwald@gmail.com>
2018-05-14 21:09:47 +0200

    added tarjan's algorithm for sccs

---
+++ src/algorithm/StronglyConnectedComponents.java
@@ -0,0 +1,94 @@
+package algorithm;
+
+import data.Edge;
+import utility.GraphUtility;
+
+import java.util.*;
+
+// this class computes the strongly connected components of a given directed graph
+public class StronglyConnectedComponents {
+
+    // the graph's adjacency list
+    private Vector<List<Edge>> adjacencyList;
+
+    // fields used in the computation
+    private int[] visitCounter;
+    private int[] lowestNodeReachable;
+    private boolean[] visited;
+    private Stack<Integer> nodeStack;
+    private int searchCounter = 0;
+
+    // result field
+    private LinkedList<Set<Integer>> stronglyConnectedComponents = new LinkedList<>();
+
+    // construct with adjacency list
+    public StronglyConnectedComponents(Vector<List<Edge>> adjacencyList) {
+        this.adjacencyList = adjacencyList;
+    }
+
+    // construct with adjacency matrix
+    public StronglyConnectedComponents(boolean[][] edges) {
+        this(GraphUtility.adjacencyMatrixToAdjacencyList(edges));
+    }
+
+    // compute SCCs using Tarjan's algorithm
+    public void computeStronglyConnectedComponents() {
+        int n = this.adjacencyList.size();
+        this.visited = new boolean[n];
+        this.visitCounter = new int[n];
+        Arrays.fill(this.visitCounter, -1);
+        this.lowestNodeReachable = new int[n];
+        this.searchCounter = 0;
+        this.nodeStack = new Stack<>();
+
+        for (int i = 0; i < n; ++i) {
+            if (visitCounter[i] == -1) {
+                tarjan(i);
+            }
+        }
+    }
+
+    // recursive depth first search using tarjan's algorithm
+    private void tarjan(int i) {
+        visitCounter[i] = this.searchCounter;
+        lowestNodeReachable[i] = this.searchCounter;
+        this.searchCounter++;
+        nodeStack.push(i);
+        visited[i] = true;
+
+        // depth first search and update lowest node reachable
+        for (Edge edge : this.adjacencyList.get(i)) {
+            int target = edge.target;
+            if (visitCounter[target] == -1) {
+                tarjan(target);
+            }
+            if (visited[target]) {
+                lowestNodeReachable[i] = Math.min(lowestNodeReachable[i], lowestNodeReachable[target]);
+            }
+        }
+
+        // triggered at root of SCC
+        if (visitCounter[i] == lowestNodeReachable[i]) {
+            this.stronglyConnectedComponents.add(new HashSet<>());
+            int j;
+            // pop the SCC
+            do {
+                j = nodeStack.pop();
+                visited[j] = false;
+                this.stronglyConnectedComponents.getLast().add(j);
+            } while (i != j);
+        }
+
+    }
+
+    // get the number of SCCs
+    public int getNumberOfStronglyConnectedComponents() {
+        return this.stronglyConnectedComponents.size();
+    }
+
+    // get list of the SCCs
+    public List<Set<Integer>> getStronglyConnectedComponents() {
+        return this.stronglyConnectedComponents;
+    }
+
+}
