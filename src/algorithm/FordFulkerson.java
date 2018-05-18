e12b2482a355c75535dc1cd407b3c9c8cc4c4710
Lukas <dev.lukas.huwald@gmail.com>
2018-05-18 09:56:53 +0200

    refactored ford fulkerson to work with abitrary source and sink

--- src/algorithm/FordFulkerson.java
+++ src/algorithm/FordFulkerson.java
@@ -10,6 +10,9 @@ public class FordFulkerson {
 
     // capacity array
     private int[][] capacities;
+    // source and sink
+    private int source;
+    private int sink;
 
     // result fields
     private int[][] flow;
@@ -18,42 +21,45 @@ public class FordFulkerson {
     // initialize Ford Fulkerson with array of capacities
     // the first index (0) is the source of the network, and the last index is the sink
     public FordFulkerson(int[][] capacities) {
+        this(capacities, 0, capacities.length-1);
+    }
+    // initialize ford fulkerson with capacities and source and sink
+    public FordFulkerson(int[][] capacities, int source, int sink) {
         this.capacities = capacities;
         int n = capacities.length;
         this.flow = new int[n][n];
+        this.source = source;
+        this.sink = sink;
     }
 
     // calculate maxFlow value and capacities
     public void computeMaxFlow() {
         int[] parents = bfs();
-        int sink = this.capacities.length - 1;
-        while (parents[sink] != -1) {
+        while (parents[this.sink] != -1) {
             int increase = this.getIncrease(parents);
             this.increasePath(parents, increase);
             parents = bfs();
         }
         this.maxFlow = 0;
         for (int i = 0; i < capacities.length; ++i) {
-            this.maxFlow += this.flow[0][i];
+            this.maxFlow += this.flow[this.source][i];
         }
     }
 
     // perform bfs from source to sink in residual network, return parents for each discovered node
     private int[] bfs() {
-        int source = 0;
-        int sink = this.capacities.length - 1;
         Queue<Integer> queue = new LinkedList<>();
         int[] parents = new int[this.capacities.length];
         Arrays.fill(parents, -1);
-        parents[source] = 0;
-        queue.add(source);
+        parents[this.source] = 0;
+        queue.add(this.source);
         while (!queue.isEmpty()) {
             int top = queue.poll();
-            if (this.capacities[top][sink] - this.flow[top][sink] > 0) {
-                parents[sink] = top;
+            if (this.capacities[top][this.sink] - this.flow[top][this.sink] > 0) {
+                parents[this.sink] = top;
                 return parents;
             } else {
-                for (int i = 0; i < sink; ++i) {
+                for (int i = 0; i < this.capacities.length; ++i) {
                     if (this.capacities[top][i] - this.flow[top][i] > 0 && parents[i] == -1) {
                         queue.add(i);
                         parents[i] = top;
@@ -65,8 +71,7 @@ public class FordFulkerson {
     }
 
     private void increasePath(int[] parents, int increase) {
-        int sink = this.capacities.length - 1;
-        int current = sink;
+        int current = this.sink;
         while (parents[current] != current) {
             int previous = parents[current];
             this.flow[previous][current] += increase;
@@ -77,8 +82,7 @@ public class FordFulkerson {
 
     private int getIncrease(int[] parents) {
         int increase = Integer.MAX_VALUE;
-        int sink = this.capacities.length - 1;
-        int current = sink;
+        int current = this.sink;
         while (parents[current] != current) {
             int previous = parents[current];
             increase = Math.min(increase, this.capacities[previous][current] - this.flow[previous][current]);
